49a7a52f308521e41cbf81880a449d51face2633
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-10 14:28:49 -0500

    my favorite number

---
+++ SelectionStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 11;
+            Console.WriteLine("Guess my favorite number.");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favoriteNumber)
+            {
+                Console.WriteLine($"{userInput} is too low.");
+            }
+            else if(userInput > favoriteNumber)
+            {
+                Console.WriteLine($"{userInput} is too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }
+}
+
